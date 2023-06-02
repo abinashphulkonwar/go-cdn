646557950aa2c2f435d4d816b1b1b4ac4f4e2cb8
abinash <abinashphulkonwar1@gmail.com>
2023-06-02 21:26:30 +0530

    add worker cdn

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"github.com/abinashphulkonwar/go-cdn/api"
 	"github.com/abinashphulkonwar/go-cdn/service"
 	"github.com/abinashphulkonwar/go-cdn/storage"
+	"github.com/abinashphulkonwar/go-cdn/worker"
 )
 
 func main() {
 	service.InitDir(storage.TempDir)
 	service.InitDir(storage.MetaDir)
 	api.App()
+	worker.Worker(storage.MetaDir)
 }
