39fda1a30ce780a6a3845977923420e25d087b62
abinash <abinashphulkonwar1@gmail.com>
2023-06-03 20:56:14 +0530

    add accepted method check

---
+++ api/handlers/methods-check.go
@@ -0,0 +1,23 @@
+package handlers
+
+import (
+	"github.com/abinashphulkonwar/go-cdn/service"
+	"github.com/gofiber/fiber/v2"
+)
+
+func MetaDataCheckHandler(c *fiber.Ctx, config service.Config) error {
+
+	method := c.Method()
+
+	_, isFound := config.Method[method]
+
+	if !isFound {
+		return &fiber.Error{
+			Code:    404,
+			Message: "Method not found",
+		}
+	}
+
+	return c.Next()
+
+}
