3689dc59a51930b9ebec6f958382295347795e9d
abinash <abinashphulkonwar1@gmail.com>
2023-06-04 21:20:51 +0530

    add method chack middleware

---
+++ api/middleware/methods-check.go
@@ -0,0 +1,23 @@
+package middleware
+
+import (
+	"github.com/abinashphulkonwar/go-cdn/service"
+	"github.com/gofiber/fiber/v2"
+)
+
+func MethodCheckHandler(c *fiber.Ctx, config service.Config) error {
+
+	method := c.Method()
+
+	_, isFound := config.Method[method]
+
+	if !isFound {
+		return &fiber.Error{
+			Code:    404,
+			Message: "Method not found",
+		}
+	}
+
+	return c.Next()
+
+}
